c9d7261b6fff545d18ee1a1f027132892736b56f
Van1s1mys <malkovsin@gmail.com>
2025-02-25 19:35:51 +0500

    #SMUZI17: Слепил предварительную версию

---
+++ src/app/commands/page.tsx
@@ -0,0 +1,9 @@
+import { CommandList } from './ui/command-list';
+
+export default function pages() {
+  return (
+    <div className="w-full md:w-1/2 ml-auto mr-auto mt-2">
+      <CommandList />
+    </div>
+  );
+}
