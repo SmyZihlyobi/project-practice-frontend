f845b03513a076745b74c152cc7ab2e972e2cee9
Van1s1mys <malkovsin@gmail.com>
2025-03-15 20:32:17 +0500

    Сосал фикс

--- src/lib/censor/ru-censor.ts
+++ src/lib/censor/ru-censor.ts
@@ -1,5 +1,5 @@
 import { RuCensor } from 'russian-bad-word-censor';
 
-RuCensor.addBadWordPattern('[с][о][с][а][л]');
-RuCensor.addBadWordPattern('[Г][и][т][л][е][р]');
+RuCensor.addBadWordPattern('.*[с][о][с][а][л].*');
+RuCensor.addBadWordPattern('.*[г][и][т][л][е][р].*');
 export const ruCensor = RuCensor;
