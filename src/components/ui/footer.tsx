e46f5df639de9825c79695b52e0e98f7eb545ff2
HollyGooffy <nikita.byc@mail.ru>
2025-05-22 18:11:19 +0500

    #SMUZI82:

--- src/components/ui/footer.tsx
+++ src/components/ui/footer.tsx
@@ -10,7 +10,7 @@ import {
 function Footer() {
   return (
     <footer className="py-8">
-      <div className="container mx-auto px-4">
+      <div className="container mx-auto px-4 pb-12">
         <div className="text-center">
           <Dialog>
             <DialogTrigger asChild>
